4a5a96e11fc512c591b5cbc1c3e865f6a074d75c
Kalynn Powell <128936392+kmarie0420@users.noreply.github.com>
2023-05-11 15:56:10 -0500

    Fixed timer bug in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,42 +15,42 @@ var timeLeft = 90;
 var timerEl = document.getElementById("timer");
 var header = document.getElementById("header");
 var scorestore = document.getElementById("scorestore");
-var back = document.getElementById("back");
+var backBtn = document.getElementById("back");
 var clear = document.getElementById("clear");
 var scores = document.getElementById("scores");
 var initials = document.getElementById("initials");
 var submitscore = document.getElementById("Assessment-Done");
 var timerId;
 var Questions = [
-    {
-      question: " What adds functionality and interactivity to a webpage?",
-      options: ["1. JavaScript", "2. HTML", "3.HTTPS", "4. jQuery"],
-      answer: "1. JavaScript",
-    },
-  
-    {
-      question: "The condition in an if/else statement is enclosed with _____.",
-      options: [
-        "1. square brackets",
-        "2. parenthesis",
-        "3. curly brackets",
-        "4. quotes",
-      ],
-      answer: "3. curly brackets",
-    },
-  
-    {
-      question: "A Javascript function is executed when something ____ it?",
-      options: ["1. provokes", "2. beckons", "3. pushes", "4. invokes"],
-      answer: "4. invokes",
-    },
-  
-    {
-      question: "Which command does not create a variable",
-      options: ["1. const", "2. let", "3. var", "4. DOM"],
-      answer: "4. DOM",
-    },
-  ];
+  {
+    question: " What adds functionality and interactivity to a webpage?",
+    options: ["1. JavaScript", "2. HTML", "3.HTTPS", "4. jQuery"],
+    answer: "1. JavaScript",
+  },
+
+  {
+    question: "The condition in an if/else statement is enclosed with _____.",
+    options: [
+      "1. square brackets",
+      "2. parenthesis",
+      "3. curly brackets",
+      "4. quotes",
+    ],
+    answer: "3. curly brackets",
+  },
+
+  {
+    question: "A Javascript function is executed when something ____ it?",
+    options: ["1. provokes", "2. beckons", "3. pushes", "4. invokes"],
+    answer: "4. invokes",
+  },
+
+  {
+    question: "Which command does not create a variable",
+    options: ["1. const", "2. let", "3. var", "4. DOM"],
+    answer: "4. DOM",
+  },
+];
 
 function startQuiz() {
   beginEl.setAttribute("class", "hide");
@@ -65,10 +65,23 @@ function updateQuestion() {
   questionAnswer4.textContent = Questions[currentQuestion].options[3];
 }
 
+function countDown() {
+    setInterval(function() {
+        timeLeft--
+        timerEl.innerHTML = "Time Left: " + timeLeft
+        if(timeLeft <= 0) {
+            timerEl.innerHTML = "Time Up!";
+            score = score
+            displayScore(score)
+        }
+    },1000)  
+}
+
 startBtn.addEventListener("click", function (event) {
   startBtn.classList.add("hide");
   QuestionsEl.classList.remove("hide");
   updateQuestion();
+  countDown()
 });
 
 questionAnswer1.addEventListener("click", function (event) {
@@ -147,21 +160,6 @@ questionAnswer4.addEventListener("click", function (event) {
   }
 });
 
-// function startTimer() {
-//     timerEl = setInterval(clockTick, 1000)
-//     {
-//     timerEl.textContent = time;
-
-// if (timeLeft < 0) {
-//     displayScore(score);
-//     clearInterval(timeInterval);
-// } 1000;}
-
-// getQuestion();
-
-// var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-// }
 
 function getQuestion() {
   var currentQuestion = Questions[currentQuestion];
@@ -174,31 +172,32 @@ function getQuestion() {
 
 // startBtn.onclick = startTimer
 
-// function clockTick() {
-//     time--;
-//     timerEl.textContent = time;
 
-//     if (time <= 0) {
-//       quizEnd();
-//     }
-//   }
 
-var submitBtn = document.getElementById("submit")
+var submitBtn = document.getElementById("submit");
 
 function displayScore(score) {
   QuestionsEl.classList.add("hide");
   submitscore.classList.remove("hide");
-  document.getElementById("score").innerHTML = 'Your score: ' + score
+  document.getElementById("score").innerHTML = "Your score: " + score;
 }
 
-submitBtn.addEventListener("click", function() {
+submitBtn.addEventListener("click", function () {
   var person = initials.value;
-  console.log({person, score})
-})
+  let whatToSave = {
+    person,
+    score,
+  };
+  localStorage.setItem("score", JSON.stringify(whatToSave));
+});
 
+function scorestore(s) {
+    submitscore.classList.add("hide");
+    highscore.classList.remove("hide")
+    document.getElementById("scorestore").innerHTML = "View Hishscores: " + scores;
+}
 
-
-// function scorestore(s) {
-//     submitscore.classList.add("hide");
-//     highscore.classList.remove("hide")
-// }
+backBtn.addEventListener("click", function(event){
+    var click = Welcome.innerHTML;
+        if (click === Welcome)
+    Welcome.textContent= "Welcome"});
